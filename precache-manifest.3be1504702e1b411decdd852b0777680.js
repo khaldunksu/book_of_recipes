494b772a56919cd37188c99c266e4ed6979a08fc
ksukhaldun <kseniya.khaldun@gmail.com>
2020-02-27 19:04:26 +0300

    Updates

---
+++ precache-manifest.3be1504702e1b411decdd852b0777680.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6447af999141e7401e21bd1b2fc5d81a",
+    "url": "/book_of_recipes/index.html"
+  },
+  {
+    "revision": "48fea9480ef9f3e1d8b2",
+    "url": "/book_of_recipes/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "504417d376ec1bec7bff",
+    "url": "/book_of_recipes/static/js/2.053178be.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/book_of_recipes/static/js/2.053178be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48fea9480ef9f3e1d8b2",
+    "url": "/book_of_recipes/static/js/main.a09a6057.chunk.js"
+  },
+  {
+    "revision": "bbafded8d0e9227f93e4",
+    "url": "/book_of_recipes/static/js/runtime-main.60fd8c7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/book_of_recipes/static/media/logo.5d5d9eef.svg"
+  }
+]);
